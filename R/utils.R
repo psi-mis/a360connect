50ff8680029b7510e1a9ae039821c6ae21855c0d
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2021-07-16 18:31:25 +0300

    fix typos

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,9 @@
 ua <- httr::user_agent("https://github.com/psi-mis/a360connect")
-timeout <- httr::timeout(600)
+timeout <- httr::timeout(60)
 
 #' @importFrom httr http_type http_error content status_code
 #' @importFrom jsonlite fromJSON
-check_response <- function(res = NULL){
+check_for_response <- function(res = NULL){
   if (!is.null(res)){
     if (http_type(res) != "application/json"){
       stop("psi-mis did not return json", call. = F)
@@ -20,5 +20,16 @@ check_response <- function(res = NULL){
   }
 }
 
+check_for_authentication <- function(user = NULL, pass = NULL){
+  if (is.null(user) && is.null(pass)){
+    user <- Sys.getenv("C_USER")
+    pass <- Sys.getenv("C_PASS")
+
+    if (user == "" || pass == ""){
+      stop("a360connect: no authenication key found, please specify")
+    }
+  }
+  list(user = user, pass = pass)
+}
 
 
