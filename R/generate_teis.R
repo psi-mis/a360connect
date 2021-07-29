e0cf10a569739ec85a44ff4d54eceababb3f8f17
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2021-07-29 09:30:15 +0300

    cleaning

--- R/generate_teis.R
+++ R/generate_teis.R
@@ -22,38 +22,6 @@ generate_teis <- function(baseurl = NULL, program_id = NULL, warn_duplicates = T
 
 
 
-#' Transform event data values
-#'
-#' @param events A data.frame object.
-transform_event_datavalues <- function(events){
-  # flatten the data values
-  new_datavalues <- purrr::map(events$dataValues, function(x){
-    if (!is.null(x) && length(x) > 0){
-      datavalues <- tidyr::pivot_wider(x, id_cols = dataElement, names_from = dataElement, values_from = value)
-    }
-  })
-  events$dataValues2 <- new_datavalues
-
-  # Add the event, date, and, status, orgUnit id and name into the data values
-  events_split <- split(events, as.numeric(row.names(events)))
-  transformed_datavalues <- purrr::map(events_split, function(x){
-    dplyr::mutate(x$dataValues2[[1]],
-                  orgUnit = x$orgUnit,
-                  orgUnitName = x$orgUnitName,
-                  event_id = x$event,
-                  eventDate = x$eventDate,
-                  status = x$status)
-  })
-
-  dv <- dplyr::bind_rows(transformed_datavalues)
-  # rename
-  names(dv) <- plyr::mapvalues(names(dv),
-                               from = des$id,
-                               to = des$name, warn_missing = F)
-  dv
-}
-
-
 
 
 
