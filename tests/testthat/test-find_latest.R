177ac53c1f81e3762ba81d6617f73f6531757db6
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2021-07-30 19:32:55 +0300

    update docs

--- tests/testthat/test-find_latest.R
+++ tests/testthat/test-find_latest.R
@@ -12,18 +12,86 @@ test_that("Find latest events returns unique event", {
 })
 
 
-test_that("Find latest unique event identifies the latest unique event from a df of events with duplicated names",{
+test_that("Find latest by name identifies the latest unique event from a df of events with duplicated names",{
+  # test for no duplicates
   d <- lapply(testd$`Girl ID`, function(x) testd[testd$`Girl ID` == x,])
-  d1 <- find_latest_unique_event(d[[415]])
-  d1 <- data.table::rbindlist(d1)
+  d1 <- find_latest_by_name(d[[415]])
   expect_equal(d1$`Phone Number`, "7080401100")
 
-  d2 <- d[[415]]
-  d2$`Phone Number` <- c("7080401100","8080401100", NA, NA)
-  d2$`Date of Service Provision` <- c("2019-06-17", "2019-06-18", "2019-06-19", "2019-06-28")
-  d3 <- find_latest_unique_event(d2[-1,])
-  d3 <- rbindlist(d3)
-  expect_equal(d3$`Date of Service Provision`, "2019-06-28")
+  d2 <- find_latest_by_name_and_id(d[[415]])
+  expect_equal(d1,d2)
 })
 
 
+test_that("Find latest by name maintains the phone number for duplicated data", {
+  d <- lapply(testd$`Girl ID`, function(x) testd[testd$`Girl ID` == x,])
+  d1 <- d[[415]]
+  d1$`Phone Number` <- c("7080401100","8080401100", NA, NA)
+  d1$`Date of Service Provision` <- c("2019-06-17", "2019-06-18", "2019-06-19", "2019-06-28")
+  d1 <- find_latest_by_name(d1[-1,])
+  expect_equal(d1$`Date of Service Provision`, "2019-06-28")
+
+})
+
+
+test_that("Find latest by name maintains the KEY for duplicated data", {
+  d <- lapply(testd$`Girl ID`, function(x) testd[testd$`Girl ID` == x,])
+  d <- d[[415]]
+  d$`Phone Number` <- c("7080401100","8080401100", NA, NA)
+  d$`Date of Service Provision` <- c("2019-06-17", "2019-06-18", "2019-06-19", "2019-06-28")
+  # test for data with keys
+  d$`KEY` <- c(NA, NA, "3Y2ORG-GWVN-5709",NA)
+  d <- find_latest_by_name(d[-1,])
+
+  expect_equal(d$`KEY`, "3Y2ORG-GWVN-5709")
+})
+
+test_that("Find latest by name returns an error when multiple KEYS are found", {
+  d <- lapply(testd$`Girl ID`, function(x) testd[testd$`Girl ID` == x,])
+  d <- d[[415]]
+  d$`Phone Number` <- c("7080401100","8080401100", NA, NA)
+  d$`Date of Service Provision` <- c("2019-06-17", "2019-06-18", "2019-06-19", "2019-06-28")
+  # test for data with keys
+  d$`KEY` <- c(NA, NA, "3Y2ORG-GWVN-5709","I1D7K8-NJBA-9063")
+  expect_error(find_latest_by_name(d[-1,]), regexp = "Multiple KEYS found!")
+})
+
+test_that("Find latest by name returns NULL when no follow up data is found", {
+  d <- lapply(testd$`Girl ID`, function(x) testd[testd$`Girl ID` == x,])
+  d <- d[[415]]
+  d <- find_latest_by_name(d[-1,])
+  expect_equal(d, NULL)
+})
+
+
+# Test latest events
+test_that("Find latest by name and id checks for non duplicates having a phone number", {
+  d <- lapply(testd$`Girl ID`, function(x) testd[testd$`Girl ID` == x,])
+  d <- d[[415]]
+  d$`Phone Number` <- c("7080401100","8080401100", NA, NA)
+  d$`Name of girl` <- paste0(d$`Name of girl`, 1:4)
+  d <- find_latest_by_name_and_id(d)
+  expect_equal(d$`Phone Number`, c("7080401100","8080401100"))
+
+})
+
+test_that("Find latest by name and id returns NULL if the events are missing a phone number", {
+  d <- lapply(testd$`Girl ID`, function(x) testd[testd$`Girl ID` == x,])
+  d <- d[[415]]
+  d$`Phone Number` <- c(NA, NA, NA, NA)
+  d$`Name of girl` <- paste0(d$`Name of girl`, 1:4)
+  d <- find_latest_by_name_and_id(d)
+  expect_equal(d, NULL)
+})
+
+
+# Test latest events by phone number
+test_that("Find latest by phone number returns the latest unique record by phone number", {
+  d <- lapply(testd$`Girl ID`, function(x) testd[testd$`Girl ID` == x,])
+  d <- d[[415]]
+  d$`Phone Number` <- c("8080401100","8080401100", NA, NA)
+  d$`Date of Service Provision` <- c("2019-06-17", "2019-06-18", "2019-06-19", "2019-06-28")
+  d <- find_latest_by_phone_number(d)
+  expect_equal(d$`Date of Service Provision`, "2019-06-18")
+
+})
