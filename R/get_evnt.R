1ddeb2df6e1564965d05fa56792e029f1676a540
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2021-08-11 18:42:13 +0300

    code re-factoring, docs

--- R/get_evnt.R
+++ R/get_evnt.R
@@ -1,55 +1,70 @@
-#'Extract evnt from a program
+#' Extract evnt from a program
 #'
-#'Extract evnt from a DHIS2 program.
+#' Extract evnt from a DHIS2 program.
 #'
-#'@param baseurl the server url.
-#'@param program_id the program ID.
-#'@param startDate the date in `YYYY-MM-DD` format.
-#'@param endDate the date in `YYYY-MM-DD` format.
-#'@param user string. the user account used in the pass key.
-#'@param pass string. password of the user account, used in the pass key.
-#'@param paging logical. Whether to page the results or not. By default, the
+#' @param baseurl the server url.
+#' @param program_id the program ID.
+#' @param startDate the date in `YYYY-MM-DD` format.
+#' @param endDate the date in `YYYY-MM-DD` format.
+#' @param user string. the user account used in the pass key.
+#' @param pass string. password of the user account, used in the pass key.
+#' @param paging logical. Whether to page the results or not. By default, the
 #'  return evnt are paged.
-#'@param pageSize numeric. indicating the size of the page. default is `50`.
-#'@importFrom httr GET content authenticate
-#'@importFrom utils URLencode
-#'@return A S3 object containing a content, endpoint and the parsed response.
-#'@export
+#' @param pageSize numeric. indicating the size of the page. default is `50`.
+#' @importFrom httr GET content authenticate
+#' @importFrom utils URLencode
+#' @return A S3 object containing a content, endpoint and the parsed response.
+#' @export
 get_evnt <- function(baseurl = NULL, program_id = NULL,
-                        startDate = NULL, endDate = NULL,
-                        user = NULL, pass = NULL,
-                        paging = TRUE, pageSize = 50, ...){
-
-  url <- modify_evnt_endpoint(baseurl = baseurl, program_id = program_id,
-                                startDate = startDate, endDate = endDate,
-                                paging = paging, pageSize = pageSize)
+                     startDate = NULL, endDate = NULL,
+                     user = NULL, pass = NULL,
+                     paging = TRUE, pageSize = 50, ...) {
+  url <- modify_evnt_endpoint(
+    baseurl = baseurl, program_id = program_id,
+    startDate = startDate, endDate = endDate,
+    paging = paging, pageSize = pageSize
+  )
 
   auth <- check_for_authentication(user, pass)
 
-  evnt_res <- GET(URLencode(url), ua, timeout, authenticate(auth$user,auth$pass), config = list(...))
+  evnt_res <- GET(URLencode(url), ua, timeout, authenticate(auth$user, auth$pass), config = list(...))
 
   check_for_response(evnt_res)
 
-  parsed_evnt <- fromJSON(
-    content(evnt_res,"text"),
-    simplifyVector = T
-  )
-
-  structure(
-    list(content = parsed_evnt,
-         endpoint = url,
-         response = evnt_res), class ="psi-mis_api"
-  )
+  parse_api_response(evnt_res, url, simplify_vector = T)
 }
 
 #' @importFrom utils str
-print.get_evnt <- function(x, ...){
+print.get_evnt <- function(x, ...) {
   cat(sprintf("PSI-MIS <%s>\n", x$endpoint))
   str(x$content, list.len = 5, vec.len = 1)
   invisible(x)
 }
 
+#' Modify evnt Endpoint
+#'
+#' Code to modify evnt API Endpoint
+#' @inheritParams get_evnt
+#' @return url
+#' @importFrom utils URLencode
+modify_evnt_endpoint <- function(baseurl = NULL, program_id = NULL,
+                                 startDate = NULL, endDate = NULL,
+                                 paging = TRUE, pageSize = 50) {
+  if (!is.null(baseurl) && !is.null(program_id)) {
+    if (!paging) {
+      url <- paste0(baseurl, "api/evnt?paging=false&program=", program_id)
+    } else {
+      url <- paste0(baseurl, "api/evnt?paging=true&totalPages=true&program=", program_id, "&pageSize=", pageSize)
+    }
 
-
-
-
+    if (!is.null(startDate)) {
+      url <- paste0(url, "&startDate=", startDate)
+    }
+    if (!is.null(endDate)) {
+      url <- paste0(url, "&endDate=", endDate)
+    }
+    URLencode(url)
+  } else {
+    stop("a360connect: atleast both the baseurl and the program id must be specified", call. = F)
+  }
+}
