6932b994c7178564b5b0aea9850c27578a0e595c
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2021-08-11 18:41:27 +0300

    update docs, file re-organization

--- R/add_latest_evnt.R
+++ R/add_latest_evnt.R
@@ -1,49 +1,49 @@
-#' Add the latest evnt to a database
+#' Add the latest event to a database
 #'
-#' Stores the latest evnt to a database. Currently, it stores the records in a
-#' google spreadsheet.
+#' Stores the latest event to a google spreadsheet database.
 #'
-#' @param latest_evnt A data.frame object.
-#' @param ssid the ID of a google spreadsheet.
-#' @param sheet the name of the sheet
-#' @param new_sheet logical. Create a new sheet? default is FALSE, to append
-#'   data to an existing spreadsheet.
+#' @param latest_evnt A data.frame or data.table object with latest events.
+#' @param ssid ID of a google spreadsheet.
+#' @param sheet The name of the sheet in the google spreadsheet to write.
+#' @param new_sheet Logical. Should a new sheet be created? by default, the
+#'   latest events will be appended on the existing sheet.
 #' @param overwrite logical. Do you want to overwrite data on the current sheet?
 #'   default is FALSE, to append data to an existing sheet.
 #' @param ... Additional params passed to the spreadsheet's endpoint
-#'   \ref{googlespreadsheet4::gs4_create}.
+#'   \code{\link[googlesheets4:gs4_create]{gs4_create()}}
+#'
 #' @importFrom googlesheets4 gs4_create sheet_append
 #' @importFrom data.table := .N
-#' @return
+#' @return The input ssid, as an instance of
+#'   \code{\link[googlesheets4:as_sheets_id]{as_sheets_id()}}
 #' @export
-add_latest_evnt <- function(latest_evnt = NULL, ssid = NULL, sheet = NULL, new_sheet = F, overwrite = F, ...){
-  if (!is.null(latest_evnt)){
+add_latest_evnt <- function(latest_evnt = NULL, ssid = NULL, sheet = NULL, new_sheet = F, overwrite = F, ...) {
+  if (!is.null(latest_evnt)) {
     latest_evnt <- data.table::as.data.table(latest_evnt)
     # Add evnt KEYs if missing
-    if (!has_key(latest_evnt)){
+    if (!has_key(latest_evnt)) {
       # Add key
       latest_evnt <- latest_evnt[, KEY := sapply(rep(14, .N), generate_girl_uid)]
+      latest_evnt <- latest_evnt[, TEI := sapply(rep(11, .N), generate_uid)]
     }
-
   }
 
-  if (!is.null(latest_evnt) && !is.null(ssid)){
-
-    if (overwrite){
-      ssd <- googlesheets4::sheet_write(latest_evnt,ss = ssid, sheet = sheet)
+  if (!is.null(latest_evnt) && !is.null(ssid)) {
+    if (overwrite) {
+      ssd <- googlesheets4::sheet_write(latest_evnt, ss = ssid, sheet = sheet)
     } else {
       # append to an existing sheet
-      if (!is.null(sheet)){
-        ssd <- googlesheets4::sheet_append(ss = ssid,latest_evnt, sheet = sheet)
-      } else{
-        ssd <- googlesheets4::sheet_append(ss = ssid,latest_evnt, sheet = 1)
+      if (!is.null(sheet)) {
+        ssd <- googlesheets4::sheet_append(ss = ssid, latest_evnt, sheet = sheet)
+      } else {
+        ssd <- googlesheets4::sheet_append(ss = ssid, latest_evnt, sheet = 1)
       }
     }
-  } else if (!is.null(latest_evnt) && is.null(ssid)){
-    if (new_sheet){
+  } else if (!is.null(latest_evnt) && is.null(ssid)) {
+    if (new_sheet) {
       ssd <- googlesheets4::gs4_create(name = generate_random_code(), ..., sheets = latest_evnt)
     }
-  } else{
+  } else {
     ssd <- NULL
   }
   ssd
--- R/find_latest_evnt.R
+++ R/find_latest_evnt.R
@@ -1,17 +1,51 @@
-#' Find latest evnt for follow up
+#' Find latest
 #'
-#' Find the latest evnt for follow-up through a systematic search of evnt
-#' using the girl ID, name and phone number.
+#' Find the latest A360 event for follow up from a list of service provision
+#' reports stored in a google spreadsheet.
 #'
-#' @param evnt A data frame with the evnt to search from.
-#' @param from Specify a google spreadsheet ID, where to search the evnt.
+#' @details The latest event is determined using the `girl ID`, `name of girl`,
+#' and `phone number` for girls with a single report and `date of service
+#' provision` for girls with multiple reports.
+#'
+#' In cases where there are multiple reports for the same girl, but the newest
+#' is missing the phone number, the newest record is updated with any available
+#' phone number in the reports:
+#'
+#' * record 1 phone number : NA
+#'
+#' * record 2 phone number : XXX XXX XXXX
+#'
+#' * record 3 phone number : NA
+#'
+#' the newest record will be assigned the phone number : XXX XXX XXXX
+#'
+#' this is similar to cases where there different phone numbers:
+#'
+#' * record 1 phone number : NA
+#'
+#' * record 2 phone number : XXX XXX XXXX
+#'
+#' * record 3 phone number : YYY XXX YYYY
+#'
+#' the newest record will be assigned both available phone numbers : XXX XXX
+#' XXXX | YYY XXX YYYY
+#'
+#' the phone number is validated; only records with a phone number equal to or
+#' more than `10 digits` are returned.
+#' @name find_latest
+#' @return A data.table object with the latest unique events for follow up.
+NULL
+
+
+#' @describeIn find_latest Find the latest event for follow up
+#'
+#' @param evnt A data frame with events to find.
+#' @param from A google spreadsheet ID, where to search the events from.
 #' @param ... Additional params passed to the \link[googlesheets4]{read_sheet}.
-#' @return the latest unique record, a data.table.
 #' @importFrom data.table as.data.table rbindlist
 #' @export
-find_latest_evnt <- function(evnt, from = NULL, ...){
-
-  if (!is.null(from)){
+find_latest <- function(evnt, from = NULL, ...) {
+  if (!is.null(from)) {
     sheet_params <- list(...)
     evnt <- googlesheets4::read_sheet(ss = from, col_types = "c", sheet_params)
   }
@@ -23,83 +57,67 @@ find_latest_evnt <- function(evnt, from = NULL, ...){
   evnt <- find_latest_by_phone_number(evnt)
 
   evnt
-
 }
 
-#' Find latest evnt by girl id and name
-#'
-#' A systematic search of evnt to identify the latest unique evnt for follow
-#' up using the girl ID and name.
-#'
-#' @param evnt A data.frame or data.table object with the evnt to follow up.
-#'
+#' @describeIn find_latest Find the latest event by girl id and name
 #' @importFrom data.table is.data.table
-#' @return the latest unique evnt if present.
-find_latest_by_name_and_id <- function(evnt){
-  if (!is.data.table(evnt)){
+find_latest_by_name_and_id <- function(evnt) {
+  if (!is.data.table(evnt)) {
     evnt <- as.data.table(evnt)
   }
 
-  evnt_ls <- lapply(evnt$`Girl ID`, function(x){
+  evnt_ls <- lapply(evnt$`Girl ID`, function(x) {
     # filter by girl id
-    evnt_by_id <- evnt[`Girl ID` == x,]
+    evnt_by_id <- evnt[`Girl ID` == x, ]
 
-    if (evnt_by_id[,.N] == 1 && has_phone_number(evnt_by_id)){
+    if (evnt_by_id[, .N] == 1 && has_phone_number(evnt_by_id)) {
       # return the unique and latest
       evnt_by_id
-    } else if (evnt_by_id[, .N] > 1 && has_phone_number(evnt_by_id)){
+    } else if (evnt_by_id[, .N] > 1 && has_phone_number(evnt_by_id)) {
       # check for duplicates names and return latest
-      if (has_duplicate_names(evnt_by_id) && has_phone_number(evnt_by_id)){
+      if (has_duplicate_names(evnt_by_id) && has_phone_number(evnt_by_id)) {
         # get latest unique evnt
         find_latest_by_name(evnt_by_id)
       } else if (!has_duplicate_names(evnt_by_id) && has_phone_number(evnt_by_id)) {
         # these are unique clients, its by coincidence they have similar names
-        evnt_by_id[!is.na(`Phone Number`) & nchar(`Phone Number`) >= 10,]
+        evnt_by_id[!is.na(`Phone Number`) & nchar(`Phone Number`) >= 10, ]
       } else {
         NULL
       }
-
     } else {
       NULL
     }
-
   })
 
   review_search_result(evnt_ls)
-
 }
 
 
-#' Find latest evnt by name
-#'
-#' Identify the latest unique evnt from evnt with duplicated names.
-#'
-#' @param evnt the duplicated evnt, A data.frame object.
-#' @return a list with latest unique evnt
-find_latest_by_name <- function(evnt){
-  if (!is.data.table(evnt)){
+#' @describeIn find_latest Find latest the event by name
+find_latest_by_name <- function(evnt) {
+  if (!is.data.table(evnt)) {
     evnt <- as.data.table(evnt)
   }
 
   evnt <- evnt[, girl_name := review_names(`Name of girl`)]
 
-  evnt_ls <- lapply(unique(evnt$girl_name), function(x){
-    dt <- evnt[girl_name == x,]
-    if (dt[, .N] == 1 && has_phone_number(dt)){
+  evnt_ls <- lapply(unique(evnt$girl_name), function(x) {
+    dt <- evnt[girl_name == x, ]
+    if (dt[, .N] == 1 && has_phone_number(dt)) {
       dt[, -"girl_name"]
-    } else if (dt[, .N] > 1 && has_phone_number(dt)){
+    } else if (dt[, .N] > 1 && has_phone_number(dt)) {
       # review and overwrite the phone numbers
       phone_number <- dt[!is.na(`Phone Number`) & nchar(`Phone Number`) >= 10, `Phone Number`]
-      if (length(phone_number) > 1){
+      if (length(phone_number) > 1) {
         phone_number <- paste(phone_number, collapse = " | ")
       }
 
-      dt <- dt[,`Phone Number`:= rep(phone_number, .N)]
+      dt <- dt[, `Phone Number` := rep(phone_number, .N)]
 
       # copy keys
-      if (has_KEY(dt)){
+      if (has_KEY(dt)) {
         key <- dt[!is.na(`KEY`), `KEY`]
-        if (length(key) > 1){
+        if (length(key) > 1) {
           stop(sprintf(
             "Multiple KEYS found! \n
             <%s>", paste(key, collapse = "; ")
@@ -119,29 +137,24 @@ find_latest_by_name <- function(evnt){
   })
 
   review_search_result(evnt_ls)
-
 }
 
-#' Find latest evnt by phone number
-#'
-#' @param evnt A data.frame or data.table object containing the latest evnt.
-#' @return the latest evnt by phone number.
-find_latest_by_phone_number <- function(evnt){
-  if (!is.data.table(evnt)){
+#' @describeIn find_latest Find the latest event by phone number
+find_latest_by_phone_number <- function(evnt) {
+  if (!is.data.table(evnt)) {
     evnt <- data.table::as.data.table(evnt)
   }
-  evnt_ls <- lapply(evnt$`Phone Number`, function(x){
+  evnt_ls <- lapply(evnt$`Phone Number`, function(x) {
     d <- evnt[`Phone Number` == x]
 
-    if (d[,.N] == 1){
+    if (d[, .N] == 1) {
       d
-    } else if (d[,.N] > 1){
+    } else if (d[, .N] > 1) {
       d <- d[order(-`Date of Service Provision`)]
-      d[1,]
-    } else{
+      d[1, ]
+    } else {
       NULL
     }
   })
   review_search_result(evnt_ls)
 }
-
