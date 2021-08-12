98fbd60de7f8cfba2e7b2edf5ed53d404bc17019
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2021-08-12 20:44:09 +0300

    rename_function generate_payload

--- R/import_teis.R
+++ R/import_teis.R
@@ -107,7 +107,7 @@ create_and_update_teis <- function(teis, ...) {
 #' }
 #' @export
 create_teis <- function(teis, baseurl = NULL, user = NULL, pass = NULL) {
-  payload <- generate_payload(teis)
+  payload <- generate_tei_payload(teis)
 
   if (is.null(baseurl)) {
     baseurl <- Sys.getenv("BASEURL")
@@ -155,7 +155,7 @@ print.create_teis <- function(x, ...) {
 #' }
 #' @export
 update_teis <- function(teis, baseurl = NULL, user = NULL, pass = NULL) {
-  payload <- generate_payload(teis)
+  payload <- generate_tei_payload(teis)
 
   if (is.null(baseurl)) {
     baseurl <- Sys.getenv("BASEURL")
@@ -163,7 +163,7 @@ update_teis <- function(teis, baseurl = NULL, user = NULL, pass = NULL) {
 
   # update
   res <- pb_lapply(payload$trackedEntity, function(x) {
-    url <- paste0(baseurl, "api/trackedEntityInstances")
+    url <- paste0(baseurl, "api/trackedEntityInstances/", x)
     url <- URLencode(url)
 
     auth <- check_for_authentication(user, pass)
@@ -203,9 +203,89 @@ get_tei <- function(tei_id, baseurl = NULL, user = NULL, pass = NULL) {
   auth <- check_for_authentication(user, pass)
 
   res <- httr::GET(
-    paste0(baseurl, "api/trackedEntityInstances", tei_id), ua, timeout,
+    paste0(baseurl, "api/trackedEntityInstances/", tei_id), ua, timeout,
     authenticate(auth$user, auth$pass)
   )
 
   res
 }
+
+#' Generate TEI payload
+#'
+#' Create a tracked entity instance payload from a line list
+#'
+#' @param evnt A list of events to generate a payload.
+#' @return A list, TEI payload
+generate_tei_payload <- function(evnt) {
+  if (has_KEY(evnt)) {
+    evnt <- as.data.table(evnt)
+    tei <- data.frame(
+      trackedEntityType = "XV3kldsZq0H",
+      trackedEntity = evnt$TEI,
+      orgUnit = evnt$orgUnit,
+      stringsAsFactors = F
+    )
+
+    # attributes
+    tei$attributes <- pb_lapply(tei$trackedEntity, function(x) {
+      dt <- data.table(
+        attribute = selected_tea,
+        value = clear_names(
+          sapply(selected_tea, function(y) {
+            evnt[TEI == x, ..y]
+          })
+        ), stringsAsFactors = F
+      )
+
+      # transform dt attributes
+      dt[, attribute := plyr::mapvalues(dt$attribute,
+        from = tea_map$name,
+        to = tea_map$id,
+        warn_missing = F
+      )]
+    })
+
+    # enrollments
+    tei$enrollments <- pb_lapply(tei$trackedEntity, function(x) {
+      dt <- evnt[TEI == x, ]
+
+      if (dt[, !is.na(`Date of follow up call`)]) {
+        data.frame(
+          orgUnit = dt[, orgUnit],
+          program = Sys.getenv("PROGRAM"),
+          enrollmentDate = dt[, `Date of follow up call`],
+          incidentDate = dt[, `Date of Service Provision`]
+        )
+      } else {
+        NULL
+      }
+    })
+
+    # filter out the empty teis
+    if (any(is_empty(tei$enrollments))){
+      warning("skipping teis missing enrollment", call. = F)
+      tei <- dplyr::filter(tei, !is_empty(enrollments))
+    }
+
+    tei
+  }
+}
+
+selected_tea <- c(
+  "Name of girl", "Girl ID", "Age of Girl",
+  "Phone Number", "Method taken up", "Date of Service Provision",
+  "Follow-up scheduled (date)", "KEY"
+)
+
+tea_map <- data.frame(
+  id = c(
+    "kn2Zlia7UOw", "JM9qqwDihBV", "zRA08XEYiSF", "SdCcqjOjop1",
+    "NEQTN6cdkWu", "oRFL3LnJrRW", "UikwH4obGZJ", "LoGHwYUQZ9y"
+  ),
+  name = c(
+    "Girl ID", "Age of Girl", "KEY",
+    "Date of Service Provision", "Method taken up", "Phone Number",
+    "Follow-up scheduled (date)", "Name of girl"
+  ),
+  stringsAsFactors = F
+)
