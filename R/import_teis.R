69eb874112117b666b177ff42d88510886b5283c
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2021-08-19 21:03:15 +0300

    make import_tei more general purpose

--- R/import_teis.R
+++ R/import_teis.R
@@ -34,6 +34,8 @@
 #' @param ... Additional params passed to [create_teis()] and [update_teis()].
 #' @return An S3 object with a DHIS2 response, API endpoint and content of the
 #'   parsed response.
+#' @importFrom httr content_type_json
+#' @importFrom jsonlite toJSON
 #' @name import_teis
 NULL
 
@@ -54,7 +56,9 @@ NULL
 #' }
 #' @export
 import_teis <- function(teis,
-                        strategy = c("create_and_update", "create", "update"),
+                        strategy = c("create_and_update",
+                                     "create",
+                                     "update"),
                         ...) {
   switch(strategy,
     create_and_update = create_and_update_teis(teis, ...),
@@ -76,16 +80,17 @@ import_teis <- function(teis,
 #' }
 #' @export
 create_and_update_teis <- function(teis, ...) {
-  res <- pb_lapply(teis$trackedEntity, function(x) {
+  p <- generate_tei_payload(teis)
+  res <- pb_lapply(p$trackedEntityInstance, function(x) {
     res1 <- get_tei(x, ...)
-    tei <- subset(teis, teis$trackedEntity == x)
+    tei <- subset(teis, teis$TEI == x)
+
     if (http_error(res1)) {
       create_teis(tei, ...)
     } else {
       update_teis(tei, ...)
     }
   })
-
   res
 }
 
@@ -97,6 +102,11 @@ create_and_update_teis <- function(teis, ...) {
 #' @param baseurl The DHIS2 server, default is psi-clone server.
 #' @param user DHIS2 User Account, used in the pass key.
 #' @param pass Password of the user account, used in the pass key.
+#' @param tracked_entity_type DHIS2 ID, the type of tracked entity attribute.
+#' @param tea A vector, specifies the tracked attributes.
+#' @param tea_mapping A data.frame object containing the tracked entity attributes and their uids.
+#' @param is_a360connect_type Logical. Use this to bypass `a360connect` settings.
+#' @param is_a360connect_enrollment_type Logical, Use this to bypass `a360connect` settings.
 #' @examples
 #' \dontrun{
 #' create_teis(teis)
@@ -106,17 +116,31 @@ create_and_update_teis <- function(teis, ...) {
 #' create_teis(teis, baseurl = server_url, user = user, pass = pass)
 #' }
 #' @export
-create_teis <- function(teis, baseurl = NULL, user = NULL, pass = NULL) {
+create_teis <- function(teis,
+                        baseurl = NULL,
+                        user = NULL,
+                        pass = NULL,
+                        tracked_entity_type = NULL,
+                        tea = NULL,
+                        tea_mapping = NULL,
+                        is_a360connect_type = TRUE,
+                        is_a360connect_enrollment_type = TRUE) {
+  output_progress("Creating tracked entity instances",
+                  cli_fun = "cli_h2")
+
   payload <- generate_tei_payload(teis)
 
   if (is.null(baseurl)) {
     baseurl <- Sys.getenv("BASEURL")
   }
 
-  url <- paste0(baseurl, "api/trackedEntityInstances")
+  url <- paste0(baseurl, "api/trackedEntityInstances?importStrategy=CREATE")
   url <- URLencode(url)
 
   auth <- check_for_authentication(user, pass)
+
+  output_progress("Creating tracked entity instances",
+                  cli_fun = "cli_alert_info")
 
   httr::POST(url,
     ua, timeout, authenticate(auth$user, auth$pass),
@@ -129,7 +153,19 @@ create_teis <- function(teis, baseurl = NULL, user = NULL, pass = NULL) {
     content_type_json()
   ) -> tei_res
 
-  parse_api_response(tei_res, url)
+  output_progress("Created tracked entity instances successively",
+                  cli_fun = "cli_alert_success",
+                  crayon_fun = "green")
+
+  output_progress("parsing api response",
+                  cli_fun = "cli_alert_info")
+
+  d <- parse_api_response(tei_res, url)
+
+  output_progress("parsed api response successively",
+                  cli_fun = "cli_alert_success",
+                  crayon_fun = "green")
+  d
 }
 
 #' @importFrom utils str
@@ -145,6 +181,11 @@ print.create_teis <- function(x, ...) {
 #' @param baseurl The DHIS2 server, default is psi-clone server.
 #' @param user DHIS2 User Account, used in the pass key.
 #' @param pass Password of the user account, used in the pass key.
+#' @param tracked_entity_type DHIS2 ID, the type of tracked entity attribute.
+#' @param tea A vector, specifies the tracked attributes.
+#' @param tea_mapping A data.frame object containing the tracked entity attributes and their uids.
+#' @param is_a360connect_type Logical. Use this to bypass `a360connect` settings.
+#' @param is_a360connect_enrollment_type Logical, Use this to bypass `a360connect` settings.
 #' @examples
 #' \dontrun{
 #' update_teis(teis)
@@ -154,7 +195,18 @@ print.create_teis <- function(x, ...) {
 #' update_teis(teis, baseurl = server_url, user = user, pass = pass)
 #' }
 #' @export
-update_teis <- function(teis, baseurl = NULL, user = NULL, pass = NULL) {
+update_teis <- function(teis,
+                        baseurl = NULL,
+                        user = NULL,
+                        pass = NULL,
+                        tracked_entity_type = NULL,
+                        tea = NULL,
+                        tea_mapping = NULL,
+                        is_a360connect_type = TRUE,
+                        is_a360connect_enrollment_type = TRUE) {
+  output_progress("updating tracked entity instances",
+                  cli_fun = "cli_h2")
+
   payload <- generate_tei_payload(teis)
 
   if (is.null(baseurl)) {
@@ -162,27 +214,40 @@ update_teis <- function(teis, baseurl = NULL, user = NULL, pass = NULL) {
   }
 
   # update
-  res <- pb_lapply(payload$trackedEntity, function(x) {
-    url <- paste0(baseurl, "api/trackedEntityInstances/", x)
+  output_progress("updating tracked entity instances",
+                  cli_fun = "cli_alert_info")
+  res <- pb_lapply(payload$trackedEntityInstance, function(x) {
+    url <- paste0(baseurl, "api/trackedEntityInstances/", x, "?importStrategy=UPDATE&ignoreEmptyCollection=true")
     url <- URLencode(url)
+
+    tei <- payload[payload$trackedEntityInstance == x,]
 
     auth <- check_for_authentication(user, pass)
 
     httr::PUT(url,
       ua, timeout, authenticate(auth$user, auth$pass),
       body = toJSON(
-        list(
-          trackedEntityInstances = payload
-        ),
+        parse_tei_payload(tei),
         auto_unbox = T
       ),
       content_type_json()
     )
   })
 
+  output_progress("updated tracked entity instances successively",
+                  cli_fun = "cli_alert_success",
+                  crayon_fun = "green")
+
+  output_progress("parsing api responses",
+                  cli_fun = "cli_alert_info")
+
   res <- res[!is_empty(res)]
 
   d <- pb_lapply(res, function(x) parse_api_response(x, x$url))
+
+  output_progress("parsed api responses successively",
+                  cli_fun = "cli_alert_success",
+                  crayon_fun = "green")
 
   d
 }
@@ -193,7 +258,6 @@ update_teis <- function(teis, baseurl = NULL, user = NULL, pass = NULL) {
 #' @param baseurl The DHIS2 server, default is psi-clone server.
 #' @param user DHIS2 User Account, used in the pass key.
 #' @param pass Password of the user account, used in the pass key.
-#'
 #' @return A DHIS2 response
 get_tei <- function(tei_id, baseurl = NULL, user = NULL, pass = NULL) {
   if (is.null(baseurl)) {
@@ -214,20 +278,74 @@ get_tei <- function(tei_id, baseurl = NULL, user = NULL, pass = NULL) {
 #'
 #' Create a tracked entity instance payload from a line list
 #'
-#' @param evnt A list of events to generate a payload.
+#' @importFrom data.table data.table as.data.table
+#'
+#' @param evnt A data.frame of events to generate a payload.
+#' @param tracked_entity_type DHIS2 ID, the type of tracked entity attribute.
+#' @param tea A vector, specifies the tracked attributes.
+#' @param tea_mapping A data.frame object containing the tracked entity attributes and their uids.
+#' @param is_a360connect_type Logical. Use this to bypass `a360connect` settings.
+#' @param is_a360connect_enrollment_type Logical, Use this to bypass `a360connect` settings.
 #' @return A list, TEI payload
-generate_tei_payload <- function(evnt) {
-  if (has_KEY(evnt)) {
-    evnt <- as.data.table(evnt)
-    tei <- data.frame(
-      trackedEntityType = "XV3kldsZq0H",
-      trackedEntity = evnt$TEI,
-      orgUnit = evnt$orgUnit,
-      stringsAsFactors = F
-    )
+generate_tei_payload <- function(evnt,
+                                 tracked_entity_type = NULL,
+                                 tea = NULL,
+                                 tea_mapping = NULL,
+                                 is_a360connect_type = TRUE,
+                                 is_a360connect_enrollment_type = TRUE) {
+  output_progress("generating tei payload",
+                  cli_fun = "cli_h3")
 
-    # attributes
-    tei$attributes <- pb_lapply(tei$trackedEntity, function(x) {
+  if (!is.data.frame(evnt)){
+    stop("evnt must be a data.frame object", call. = F)
+  } else {
+    # make the event a data.table, we will use this to easily filter by columns
+    # & more efficiently compared to a df object.
+    evnt <- as.data.table(evnt)
+  }
+
+  if (is_a360connect_type){
+    if (!has_KEY(evnt)){
+      stop("evnt must have a special column,
+           `KEY`, that uniquely identifys the events to generate TEIs", call. = F)
+    }
+  }
+
+  tei <- data.frame(
+    trackedEntityType = ifelse(!is.null(tracked_entity_type),
+                               tracked_entity_type,
+                               Sys.getenv("TRACKED_ENTITY_TYPE")),
+    orgUnit = evnt$orgUnit,
+    stringsAsFactors = F
+  )
+
+  if (!has_tei_ids(evnt)){
+    stop("evnt must have a special column, `TEI`, with the TEI ids")
+  } else {
+    tei$trackedEntityInstance <- evnt$TEI
+  }
+
+  # set attributes
+  if (!is.null(tea) && !is.null(tea_mapping)){
+    tei$attributes <- lapply(tei$trackedEntityInstance, function(x) {
+      dt <- data.table(
+        attribute = tea,
+        value = clear_names(
+          sapply(tea, function(y) {
+            evnt[TEI == x, ..y]
+          })
+        ), stringsAsFactors = F
+      )
+
+      # transform dt attributes
+      dt[, attribute := plyr::mapvalues(dt$attribute,
+                                        from = tea_mapping$name,
+                                        to = tea_mapping$id,
+                                        warn_missing = F
+      )]
+    })
+  } else {
+    tei$attributes <- lapply(tei$trackedEntityInstance, function(x) {
       dt <- data.table(
         attribute = selected_tea,
         value = clear_names(
@@ -239,14 +357,43 @@ generate_tei_payload <- function(evnt) {
 
       # transform dt attributes
       dt[, attribute := plyr::mapvalues(dt$attribute,
-        from = tea_map$name,
-        to = tea_map$id,
-        warn_missing = F
+                                        from = tea_map$name,
+                                        to = tea_map$id,
+                                        warn_missing = F
       )]
     })
+  }
 
-    # enrollments
-    tei$enrollments <- pb_lapply(tei$trackedEntity, function(x) {
+  output_progress("setting tracked entity attributes",
+                  cli_fun = "cli_alert_info")
+
+  # set enrollments
+  if (!is_a360connect_enrollment_type) {
+    # for generic use case
+    tei$enrollments <- lapply(tei$trackedEntityInstance, function(x) {
+      dt <- evnt[TEI == x, ]
+
+      enrollment <- data.frame(
+        orgUnit = dt[, orgUnit],
+        program = Sys.getenv("PROGRAM"),
+        stringsAsFactors = F
+      )
+
+      if (has_incident_date(dt)) {
+        enrollment$incidentDate <- dt[, `incident_date`]
+      }
+
+      if (has_enrollment_date(dt)) {
+        enrollment$enrollmentDate <- dt[, `enrollment_date`]
+      }
+
+      enrollment
+
+    })
+
+  } else {
+    # default to A360 connect use case
+    tei$enrollments <- lapply(tei$trackedEntityInstance, function(x) {
       dt <- evnt[TEI == x, ]
 
       if (dt[, !is.na(`Date of follow up call`)]) {
@@ -260,23 +407,58 @@ generate_tei_payload <- function(evnt) {
         NULL
       }
     })
-
-    # filter out the empty teis
-    if (any(is_empty(tei$enrollments))){
-      warning("skipping teis missing enrollment", call. = F)
-      tei <- dplyr::filter(tei, !is_empty(enrollments))
-    }
-
-    tei
   }
+
+  output_progress("attaching enrollments",
+                  cli_fun = "cli_alert_info")
+
+  # filter out the empty teis
+  if (any(is_empty(tei$enrollments))) {
+    if (interactive()){
+      warning("skipping teis missing an enrollment", call. = F)
+    }
+    tei <- dplyr::filter(tei, !is_empty(enrollments))
+  }
+
+  output_progress("payload generated successively",
+                  cli_fun = "cli_alert_success",
+                  crayon_fun = "green")
+
+  tei
 }
 
+#' Selected Tracked Entity Attributes
+#'
+#' A vector containing a names of tracked entity attributes implemented in `A360`
+#' `provider Call Log Program`
+#'
+#' @format
+#' \describe{
+#'   \item{Name of girl}{name of a tracked entity attribute, captures the name of girl}
+#'   \item{Girl ID}{name of a tracked entity attribute, captures the A360 girl id}
+#'   \item{Phone Number}{name of a tracked entity attribute, captures the Phone Number}
+#'   \item{Method taken up}{name of a tracked entity attribute, records the method taken up}
+#'   \item{Date of Service Provision}{name of a tracked entity attribute, records the date of service provision}
+#'   \item{Follow-up scheduled (date)}{name of a tracked entity attribute, captures the scheduled date of the follow up call}
+#'   \item{KEY}{refers to the Unique Identifier for the TEIs, captured in `CORE - Unique ID (UIC) `}
+#' }
 selected_tea <- c(
   "Name of girl", "Girl ID", "Age of Girl",
   "Phone Number", "Method taken up", "Date of Service Provision",
   "Follow-up scheduled (date)", "KEY"
 )
 
+#' Tracked Entity Attributes data map
+#'
+#' A dataset containing the ids and name tracked entity attributes used in `A360`
+#' `Provider Call Log program`
+#'
+#' @format
+#' \describe{
+#'   \item{id}{id, Unique ID of the tracked entity attribute}
+#'   \item{name}{name,name of the tracked entity attribute}
+#' }
+#' @source \url{https://staging.psi-mis.org/}
 tea_map <- data.frame(
   id = c(
     "kn2Zlia7UOw", "JM9qqwDihBV", "zRA08XEYiSF", "SdCcqjOjop1",
@@ -289,3 +471,17 @@ tea_map <- data.frame(
   ),
   stringsAsFactors = F
 )
+
+parse_tei_payload <- function(tei){
+
+  if (is_tei_payload(tei)) {
+    list(
+      trackedEntityType = tei$trackedEntityType[1],
+      trackedEntityInstance = tei$trackedEntityInstance[1],
+      orgUnit = tei$orgUnit[1],
+      attributes = tei$attributes[[1]],
+      enrollments = tei$enrollments[[1]]
+    )
+  }
+
+}
